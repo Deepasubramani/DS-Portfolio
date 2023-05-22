08571f439c591ba5352b3eb1c37108de710cee59
Deepasubramani <47238565+Deepasubramani@users.noreply.github.com>
2023-05-22 13:25:16 +0100

    Update Index.js

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -1,7 +1,1 @@
-$(document).ready(function(){
-  $(".project__card").mouseover(function(){
-      $(".arrow").css("animation", "bounce 0.6s");
-      $(".arrow").css("animation-direction", "alternate");
-      $(".arrow").css("animation-iteration-count", "2");
-  });
-});
+
